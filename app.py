2f9c86a1f8477471ddef3c2a239401f901fd1783
ivan <oscar.delacruz19f@gmail.com>
2022-07-13 00:03:05 -0500

    'Render main templates'

--- app.py
+++ app.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
