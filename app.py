7ca5497d8c479a49a68c99cdd08643a5a0d23685
Kalincillo <82189871+Kalincillo@users.noreply.github.com>
2022-08-25 16:49:01 -0500

    Add projects placeholder

--- app.py
+++ app.py
@@ -6,33 +6,31 @@ app = Flask(__name__)
 app.config['DATABASE_URI'] = 'sqlite://///home/ivan/Projects/website/blog.db'
 db = SQLAlchemy(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
-
-@app.route('/add')
-def add():
-    return render_template('add.html')
 
 @app.route('/music')
 def music():
     return render_template('music.html')
 
+
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
 
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
